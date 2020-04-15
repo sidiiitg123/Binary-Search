d48826e2be1a4685a73c86676949b3a4d375ee28
sidiiitg123 <sudhanshuupadhyay457@gmail.com>
2020-04-15 09:00:20 +0530

    completed

---
+++ BS_SOLUTIONS/src/com/company/CountElement.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.List;
+
+public class CountElement {
+    int bSearch(List<Integer> A, boolean searchFirst, int B){
+        int low = 0;
+        int high = A.size()-1;
+        int result = -1;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(A.get(mid) == B){
+                result = mid;
+                if(searchFirst){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else if(A.get(mid) > B){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+
+        return result;
+    }
+
+    public int findCount(final List<Integer> A, int B) {
+
+        int left = bSearch(A,true,B);
+        int right = bSearch(A,false,B);
+
+        if(left == -1){
+            return 0;
+        }
+
+        return right-left + 1;
+    }
+}
